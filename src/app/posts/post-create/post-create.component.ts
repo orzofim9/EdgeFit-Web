03feada63478b0220a80a64849c77176fcaabbdb
or <orzofim9@gmail.com>
2019-08-04 11:47:58 +0300

    Initial Commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output} from '@angular/core';
+import {NgForm} from "@angular/forms";
+
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: [ './post-create.component.css' ]
+})
+export class PostCreateComponent {
+  enteredValue =  '';
+  enteredContent =  '';
+  @Output() post_created = new EventEmitter<Post>();
+
+  onAddedPost(form: NgForm) {
+    if(form.invalid) {
+      return;
+    }
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content
+    }
+    this.post_created.emit(post);
+  }
+}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+import { Post } from './post.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+ /* posts = [
+    {title: 'First title', content: 'First content'},
+    {title: 'Second title', content: 'Second content'},
+    {title: 'Third title', content: 'Third content'},
+  ];
+  */
+ @Input() posts: Post[] = [];
+}
