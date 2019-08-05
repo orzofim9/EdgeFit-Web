aca542a52029b388f42f7dab8aadb3ad31831f79
Sagi Barak <sagi2006@gmail.com>
2019-08-05 20:18:22 +0300

    signup request mechanism

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,9 +9,12 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent{
   isLoading = false;
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   onSignup(form: NgForm){
-    console.log("IN!")
+    if(form.invalid){
+      return;
+    }
+    this.authService.createUser(form.value.email,form.value.password);
   }
 }
